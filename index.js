0e5e9f0082927c127be5ec247645a89b44cf0c9a
Jothael <jothael.moya@gmail.com>
2023-08-12 05:43:19 -0300

    PRE ENTREGA 3

--- index.js
+++ index.js
@@ -1,95 +1,107 @@
-let catalogo = [];
+const catalogoKey = "catalogo";
+let catalogo = cargarCatalogoDesdeLocalStorage() || [];
 
-function agregarProducto() {
-  let agregarOtro = true;
-
-  while (agregarOtro) {
-    let nombre = prompt("Ingresa el nombre del producto:");
-    let precio = Number(prompt("Ingresa el precio del producto:"));
-    let descripcion = prompt("Ingresa la descripción del producto:");
-    let talla = prompt("Ingresa la talla del producto:");
-    let colores = prompt("Ingresa los colores disponibles del producto (separados por comas):").split(",");
-
-    if (nombre === "" || isNaN(precio) || precio <= 0 || descripcion === "" || talla === "") {
-      alert("Por favor, ingresa valores válidos para el producto.");
-    } else {
-      let producto = {
-        nombre: nombre,
-        precio: precio,
-        descripcion: descripcion,
-        talla: talla,
-        colores: colores
-      };
-
-      catalogo.push(producto);
-
-      agregarOtro = confirm("¿Deseas agregar otro producto al catálogo?");
-    }
-  }
-
-  actualizarCatalogo();
-  alert("Los productos han sido agregados con éxito.");
+function cargarCatalogoDesdeLocalStorage() {
+    const catalogoGuardado = localStorage.getItem(catalogoKey);
+    return catalogoGuardado ? JSON.parse(catalogoGuardado) : [];
 }
 
+function guardarCatalogoEnLocalStorage() {
+    localStorage.setItem(catalogoKey, JSON.stringify(catalogo));
+}
+
+function mostrarFormulario() {
+    document.getElementById("formulario").style.display = "block";
+}
+
+function agregarProducto() {
+    mostrarFormulario();
+}
+
+document.getElementById("agregar").addEventListener("click", agregarProducto);
+
+document.getElementById("guardar").addEventListener("click", () => {
+    const nombre = document.getElementById("nombre").value;
+    const precio = parseFloat(document.getElementById("precio").value);
+    const descripcion = document.getElementById("descripcion").value;
+    const talla = document.getElementById("talla").value;
+    const colores = document.getElementById("colores").value.split(",");
+
+    if (nombre && !isNaN(precio) && precio > 0 && descripcion && talla) {
+        catalogo.push({ nombre, precio, descripcion, talla, colores });
+        guardarCatalogoEnLocalStorage();
+        actualizarCatalogo();
+        document.getElementById("formulario").style.display = "none";
+
+        document.getElementById("nombre").value = "";
+        document.getElementById("precio").value = "";
+        document.getElementById("descripcion").value = "";
+        document.getElementById("talla").value = "";
+        document.getElementById("colores").value = "";
+        alert("Los productos han sido agregados con éxito.")
+    } else {
+        alert("Por favor, ingresa valores válidos para el producto.");
+    }
+});
 function buscarPorColor(colorBuscado) {
   return catalogo.filter(producto => producto.colores.includes(colorBuscado));
 }
 
 function filtrarPorColor(coloresFiltrados) {
-  return catalogo.filter(producto => producto.colores.some(color => coloresFiltrados.includes(color)));
+  return catalogo.filter(producto => coloresFiltrados.some(color => producto.colores.includes(color)));
 }
 
 function mostrarMenu() {
-  let continuar = true;
-
-  while (continuar) {
-    let opcion = prompt("¿Qué deseas hacer?\n1. Buscar producto por color\n2. Filtrar productos por color\n3. Salir");
-
-    switch (opcion) {
-      case "1":
-        let colorBuscado = prompt("Ingresa el color que deseas buscar:");
-        let productosConColor = buscarPorColor(colorBuscado);
-
-        if (productosConColor.length > 0) {
-          alert("Productos con el color " + colorBuscado + ":\n" + productosConColor.map(producto => producto.nombre).join(", "));
-        } else {
-          alert("No se encontraron productos con el color " + colorBuscado);
-        }
-
-      case "2":
-        let coloresFiltrados = prompt("Ingresa los colores que deseas filtrar (separados por comas):").split(",");
-        let productosFiltradosPorColor = filtrarPorColor(coloresFiltrados);
-
-        if (productosFiltradosPorColor.length > 0) {
-          alert("Productos con los colores filtrados:\n" + productosFiltradosPorColor.map(producto => producto.nombre).join(", "));
-        } else {
-          alert("No se encontraron productos con los colores filtrados");
-        }
-        
-
-      case "3":
-        continuar = false;
-        
-
-    }
+  let continuarMenu = true;
+  const opciones = {
+    "1": () => {
+      const colorBuscado = prompt("Ingresa el color que deseas buscar:");
+      const productosConColor = buscarPorColor(colorBuscado);
+      mostrarProductosEnAlert(productosConColor, `Productos con el color ${colorBuscado}:`);
+    },
+    "2": () => {
+      const coloresFiltrados = prompt("Ingresa los colores que deseas filtrar (separados por comas):").split(",");
+      if (coloresFiltrados.length === 0) {
+        alert("Por favor, ingresa al menos un color para filtrar.");
+      } else {
+        const productosFiltradosPorColor = filtrarPorColor(coloresFiltrados);
+        mostrarProductosEnAlert(productosFiltradosPorColor, "Productos con los colores filtrados:");
+      }
+    },
+    "3": () => {
+      continuarMenu = false;
+    },
+    "default": () => {
+      alert("Por favor, ingresa una opción válida.");
+    },
+  };
+  while (continuarMenu) {
+    const opcion = prompt("¿Qué deseas hacer?\n1. Buscar producto por color\n2. Filtrar productos por color\n3. Salir");
+    const accion = opciones[opcion] || opciones["default"];
+    accion();
   }
-
 }
+
+function mostrarProductosEnAlert(productos, mensaje) {
+  const nombresProductos = productos.map(producto => producto.nombre).join(", ");
+  const mensajeMostrar = productos.length > 0 ? `${mensaje}\n${nombresProductos}` : `No se encontraron productos con los criterios de búsqueda.`;
+  alert(mensajeMostrar);
+}
+
 
 function actualizarCatalogo() {
-  let listaCatalogo = document.getElementById("catalogo");
-  listaCatalogo.innerHTML = "";
+    const listaCatalogo = document.getElementById("catalogo");
+    listaCatalogo.innerHTML = "";
+    for (const producto of catalogo) {
+        const li = document.createElement("li");
+        li.innerHTML = `<b>${producto.nombre}</b> - $${producto.precio.toFixed(2)}<br>${producto.descripcion}<br>Talla: ${producto.talla}<br>Colores disponibles: ${producto.colores.join(", ")}`;
+        listaCatalogo.appendChild(li);
+        
+    }
 
-  for (let i = 0; i < catalogo.length; i++) {
-    let producto = catalogo[i];
-
-    let li = document.createElement("li");
-    li.innerHTML = "<b>" + producto.nombre + "</b> - $" + producto.precio.toFixed(2) + "<br>" + producto.descripcion + "<br>Talla: " + producto.talla + "<br>Colores disponibles: " + producto.colores.join(", ");
-
-    listaCatalogo.appendChild(li);
-  }
 }
 
-document.getElementById("agregar").addEventListener("click", agregarProducto);
+cargarCatalogoDesdeLocalStorage();
+actualizarCatalogo();
 
 document.getElementById("mostrarMenu").addEventListener("click", mostrarMenu);
